02469779d4309be87475f56e5b24807d55d71362
易超 <mangoyiy@163.com>
2025-08-12 18:31:31 +0800

    构建报错

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
-import type { NextConfig } from "next";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-const nextConfig: NextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
